16264d75e5dc7fddab8334bf3da9cb07b0f00642
JeonB <newjeonb@gmail.com>
2024-11-08 16:53:42 +0900

    에디터에 언어 추가

--- components/CodeEditor.tsx
+++ components/CodeEditor.tsx
@@ -1,21 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CodeMirror from '@uiw/react-codemirror'
 import { javascript } from '@codemirror/lang-javascript'
+import { java } from '@codemirror/lang-java'
+import { cpp } from '@codemirror/lang-cpp'
+import { python } from '@codemirror/lang-python'
 import { useCode } from './context/CodeContext'
+
 export default function CodeEditor() {
   const codeContext = useCode()
   const { code, setCode } = codeContext
-  const onChange = (e: React.SetStateAction<string>) => {
-    setCode(e)
+  const [language, setLanguage] = useState('javascript')
+
+  const onChange = (value: string) => {
+    setCode(value)
+  }
+
+  const handleLanguageChange = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setLanguage(event.target.value)
+  }
+
+  const getLanguageExtension = () => {
+    switch (language) {
+      case 'javascript':
+        return javascript({ jsx: true })
+      case 'java':
+        return java()
+      case 'c':
+      case 'cpp':
+        return cpp()
+      case 'python':
+        return python()
+      default:
+        return javascript({ jsx: true })
+    }
   }
 
   return (
-    <CodeMirror
-      value={code}
-      onChange={onChange}
-      height="70vh"
-      theme="dark"
-      extensions={[javascript({ jsx: true })]}
-    />
+    <div>
+      <select value={language} onChange={handleLanguageChange}>
+        <option value="javascript">JavaScript</option>
+        <option value="java">Java</option>
+        <option value="c">C</option>
+        <option value="cpp">C++</option>
+        <option value="python">Python</option>
+      </select>
+      <CodeMirror
+        value={code}
+        onChange={onChange}
+        height="70vh"
+        theme="dark"
+        extensions={[getLanguageExtension()]}
+      />
+    </div>
   )
 }
