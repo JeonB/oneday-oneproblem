3a06440b98ca889f533208c883eecae39d24af08
JeonB <newjeonb@gmail.com>
2024-12-21 16:38:43 +0900

    login page 에러 수정

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,5 @@
 import Login from '@/components/ui/login'
 
-export const description = 'A simple login form.'
-
-export const iframeHeight = '870px'
-
-export const containerClassName = 'w-full h-full'
-
 export default function Page() {
   return (
     <div className="flex h-screen w-full items-center justify-center px-4">
