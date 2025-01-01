8d6b53038398efe32372026487a35c754943c63f
JeonB <newjeonb@gmail.com>
2025-01-01 23:08:50 +0900

    스켈레톤 추가

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import Card from '@/components/ui/problem/AlgorithmCard'
 import { Button } from '@/components/ui/button'
+import { Skeleton } from '@/components/ui/skeleton'
 import { Algorithm } from '@/app/lib/models/Algorithms'
 import DifficultyModal from '@/components/ui/problem/DifficultyModal'
 import { useAlgorithmStore } from '@/components/context/Store'
@@ -15,6 +16,7 @@ const MainPage: React.FC = () => {
     null,
   )
   const [isModalOpen, setModalOpen] = useState(false)
+  const [isLoading, setIsLoading] = useState(true) // 로딩 상태 추가
 
   const getRandomDifficulty = () => {
     const difficulties = ['easy', 'normal', 'hard']
@@ -25,6 +27,7 @@ const MainPage: React.FC = () => {
   useEffect(() => {
     const fetchAlgorithms = async () => {
       try {
+        setIsLoading(true) // 로딩 상태 시작
         const response = await fetch('/api/algorithms')
         if (!response.ok) {
           throw new Error(`Failed to fetch algorithms: ${response.statusText}`)
@@ -36,6 +39,8 @@ const MainPage: React.FC = () => {
         alert(
           '알고리즘 데이터를 가져오지 못했습니다. 나중에 다시 시도해주세요.',
         )
+      } finally {
+        setIsLoading(false) // 로딩 상태 종료
       }
     }
 
@@ -53,6 +58,7 @@ const MainPage: React.FC = () => {
     setSelectedAlgorithm(algorithm)
     setModalOpen(true)
   }
+
   return (
     <div>
       <div className="mx-auto flex flex-col items-center p-8">
@@ -84,17 +90,25 @@ const MainPage: React.FC = () => {
       {/* 알고리즘 카드 리스트 섹션 */}
       <div className="mb-10 flex justify-center">
         <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-          {algorithms.map(algorithm => (
-            <Card
-              key={algorithm.topic}
-              onClick={() => handleAlgorithmClick(algorithm)}
-              topic={algorithm.topic}
-              algorithm={algorithm.name}
-              img={algorithm.img}
-            />
-          ))}
+          {isLoading
+            ? Array.from({ length: 16 }).map((_, index) => (
+                <Skeleton
+                  key={index}
+                  className="h-32 w-32 rounded-lg md:h-40 md:w-40"
+                /> // Skeleton 사용
+              ))
+            : algorithms.map(algorithm => (
+                <Card
+                  key={algorithm.topic}
+                  onClick={() => handleAlgorithmClick(algorithm)}
+                  topic={algorithm.topic}
+                  algorithm={algorithm.name}
+                  img={algorithm.img}
+                />
+              ))}
         </div>
       </div>
+
       {/* 난이도 선택 모달 */}
       <DifficultyModal
         isOpen={isModalOpen}
